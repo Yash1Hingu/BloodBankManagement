742e50a5e9c2e36a4c938cf5aff132e0d3f00435
Yash1Hingu <yash23hingu@gmail.com>
2023-07-08 19:41:30 +0530

    UPDATE -- admin login

--- app.js
+++ app.js
@@ -6,29 +6,47 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
+const USEREMAIL = 'yash23@gmail.com';
+const USERPASSWORD = 'yash123';
+let ADMININ = false;
 
 app.get("/",(req,res) => {
-    res.render("home");
+    res.render("home",{adminhome:ADMININ});
 })
 
 app.get("/about",(req,res) => {
-    res.render("about");
+    res.render("about",{adminhome:ADMININ});
 })
 
 app.get("/contact",(req,res) => {
-    res.render("contact");
+    res.render("contact",{adminhome:ADMININ});
 })
 
 app.get("/campaing",(req,res) => {
-    res.render("campaing");
+    res.render("campaing",{adminhome:ADMININ});
 })
 
 app.get("/register",(req,res) => {
-    res.render('register');
+    res.render('register',{adminhome:ADMININ});
 })
 
 app.get("/login",(req,res) => {
-    res.render('login');
+    res.render('login',{adminhome:ADMININ});
+})
+
+app.post("/loginuser",(req,res) => {
+    const useremail = req.body.useremail;
+    const userpassword = req.body.userpassword;
+
+    if(useremail === USEREMAIL && userpassword === USERPASSWORD) {
+        ADMININ = true;
+        res.render('home',{adminhome : ADMININ});
+    }
+})
+
+app.get("/logout",(req,res) => {
+    ADMININ = false;
+    res.render('home',{adminhome:ADMININ});
 })
 
 
