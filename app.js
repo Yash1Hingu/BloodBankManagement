7414fa96141aecd4147171f2fee2ca9e9a930e03
Yash1Hingu <yash23hingu@gmail.com>
2023-07-09 19:10:57 +0530

    ADD -- Request Blood Form

--- app.js
+++ app.js
@@ -1,56 +1,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const multer = require("multer");
 const app = express();
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+const upload = multer();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const USEREMAIL = 'yash23@gmail.com';
 const USERPASSWORD = 'yash123';
 let ADMININ = false;
 
-app.get("/",(req,res) => {
-    res.render("home",{adminhome:ADMININ});
+app.get("/", (req, res) => {
+    res.render("home", { adminhome: ADMININ });
 })
 
-app.get("/about",(req,res) => {
-    res.render("about",{adminhome:ADMININ});
+app.get("/about", (req, res) => {
+    res.render("about", { adminhome: ADMININ });
 })
 
-app.get("/contact",(req,res) => {
-    res.render("contact",{adminhome:ADMININ});
+app.get("/contact", (req, res) => {
+    res.render("contact", { adminhome: ADMININ });
 })
 
-app.get("/campaing",(req,res) => {
-    res.render("campaing",{adminhome:ADMININ});
+app.get("/campaing", (req, res) => {
+    res.render("campaing", { adminhome: ADMININ });
 })
 
-app.get("/register",(req,res) => {
-    res.render('register',{adminhome:ADMININ});
+app.get("/register", (req, res) => {
+    res.render('register', { adminhome: ADMININ });
 })
 
-app.get("/login",(req,res) => {
-    res.render('login',{adminhome:ADMININ});
+app.get("/login", (req, res) => {
+    res.render('login', { adminhome: ADMININ });
 })
 
-app.post("/loginuser",(req,res) => {
+app.post("/loginuser", (req, res) => {
     const useremail = req.body.useremail;
     const userpassword = req.body.userpassword;
 
-    if(useremail === USEREMAIL && userpassword === USERPASSWORD) {
+    if (useremail === USEREMAIL && userpassword === USERPASSWORD) {
         ADMININ = true;
-        res.render('home',{adminhome : ADMININ});
+        res.render('home', { adminhome: ADMININ });
     }
 })
 
-app.get("/logout",(req,res) => {
+app.get("/logout", (req, res) => {
     ADMININ = false;
-    res.render('home',{adminhome:ADMININ});
+    res.render('home', { adminhome: ADMININ });
+})
+
+app.get('/requestblood', (req, res) => {
+    res.render('requestblood');
+})
+
+app.post('/requestblood', upload.single('patientphoto'),(req, res) => {
+    const photo = req.file;
+    const patientDetails = {
+        patientfullname: req.body.patientfullname,
+        patientemail: req.body.patientemail,
+        patientpassword: req.body.patientpassword,
+        patientbloodgrp: req.body.patientbloodgrp,
+        contactnumber: req.body.contactnumber,
+        patientbirthdate: req.body.patientbirthdate,
+        patientaddress: req.body.patientaddress,
+        patientcity: req.body.patientcity,
+        patientstate: req.body.patientstate,
+        patientphoto: photo,
+    }
+    console.log(patientDetails);
 })
 
 
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server Startes on 3000 port.");
 })
